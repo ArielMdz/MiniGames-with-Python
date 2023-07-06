73260fb4a85fe18b7dd0a0cb548987cc76aa53de
Ariel Mendoza <Ariel.Mendozasdu@gmail.com>
2023-07-05 18:04:22 -0700

    Se modifico el juego Game of life para usar matriz

--- Games/Game_of_life.py
+++ Games/Game_of_life.py
@@ -9,17 +9,18 @@ YELLOW = (255, 255, 0)
 
 
 def cursor_mouse(x, y):
-    if y > 100 and y < 601 and x > 0 and x < 501:
-        return int(x/10) + (50 * int((y - 100)/10))
+    if y > 100 and y < 600 and x > 0 and x < 500:
+        return [(int(x/10)), (int((y - 100)/10))]
     else: 
-        return 3000
+        return [50, 50]
     
 
 def start_game():
-    #init_game()
-    cells = [0] * 3001
-    cells_before = [0] * 3001
-
+    #Declaracion de varaibles 
+    cells = [[0 for i in range(0, 51)] for j in range(0, 51)]
+    cells_before = [[0 for i in range(0, 51)] for j in range(0, 51)]
+    pos_game = [50, 50]
+    print(pos_game[0], pos_game[1])
     pygame.init()                               #Se crea la ventana 
     pygame.display.set_caption('Snake Game')    #Se asigna un titulo
 
@@ -35,36 +36,42 @@ def start_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game = False
-
+            #Evento de poscion de mouse
             if event.type == pygame.MOUSEMOTION:
                 pos = event.pos
                 pos_game = cursor_mouse(pos[0], pos[1])
-
+            #Evento de click de boton
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos= event.pos
+                pos = event.pos
                 pos_life = cursor_mouse(pos[0], pos[1])
-                if cells[pos_life] == 0 and not pos_life == 3000:
-                    cells[pos_life] = 1
-                elif not pos_life == 3000:
-                    cells[pos_life] = 0
-                
-        if cells[pos_game] == 0 and not pos_game == 3000:
-            cells[pos_game] = 2
+                if cells[pos_life[0]][pos_life[1]] == 0 and not pos_life == [50, 50]:
+                    cells[pos_life[0]][pos_life[1]] = 1
+                elif not pos_life == [50, 50]:
+                    cells[pos_life[0]][pos_life[1]] = 0
+        
+        #color y textos de la interfaz
         screen.fill(BLACK)
         screen.blit(text, (110, 10))
+
+        #altera el valor de la celula sobre la que se encuentra el mouse
+        if cells[pos_game[0]][pos_game[1]] == 0 and not pos_game == [50, 50]:
+            cells[pos_game[0]][pos_game[1]] = 2
         
-        for i in range(0, 500, 10):
-            for j in range(100, 600, 10):
-                index = int(i/10 + (50 * (j-100)/10))
-                if cells[index] == 0:
-                    pygame.draw.rect(screen, GRAY, (i, j, 9, 9))
-                elif cells[index] == 1:
-                    pygame.draw.rect(screen, GREEN, (i, j, 9, 9))
-                else:
-                    pygame.draw.rect(screen, YELLOW, (i, j, 9, 9))
-                    cells[index] = 0
+        #Imprime totas las celulas
+        for column in range(0, 500, 10):
+            for row in range(100, 600, 10):
+                x = int(column / 10)
+                y = int((row - 100) / 10)
+                if cells[x][y] == 0:
+                    pygame.draw.rect(screen, GRAY, (column, row, 9, 9))
+                elif cells[x][y] == 1:
+                    pygame.draw.rect(screen, GREEN, (column, row, 9, 9))
+                elif cells[x][y] == 2:
+                    pygame.draw.rect(screen, YELLOW, (column, row, 9, 9))
+                    cells[x][y] = 0
+
         pygame.display.flip()
-        clock.tick(30)
+        clock.tick(15)
 
     pygame.quit()
 
